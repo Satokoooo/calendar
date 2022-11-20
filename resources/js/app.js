9fb6c749f9f746bbc9da07c6b137224b2fe4a385
s-harada <satokotseso@gmail.com>
2022-11-20 07:02:16 +0000

    環境構築

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-import './bootstrap';
+// import './bootstrap';
+
+require("./bootstrap");
+import { createApp } from "vue";
+import App from "./components/App.vue";
+
+createApp(App).mount("#app");
